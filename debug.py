8e7dff064132e3afd03d9cb8f2d09ab05aa14047
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-06-25 23:37:11 -0400

    no hope

--- debug.py
+++ debug.py
@@ -5,5 +5,7 @@ from marginaleffects import *
 df = pl.read_csv("mtcars.csv")
 mod = smf.ols("mpg ~ wt * hp", df).fit()
 print(comparisons(mod, comparison = "difference", by = True))
+print(comparisons(mod, comparison = "difference", by = False))
+print(comparisons(mod, comparison = "difference", by = ["cyl", "gear"]))
 # print(comparisons(mod_py, comparison = "difference", by = True))
 # print(comparisons(mod_py, comparison = "ratio", by = True))
