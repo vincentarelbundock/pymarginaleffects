071aa5e9f368d6c92b3cbb587aec9deb8ec56fd5
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-02 13:49:20 -0400

    remove debug scripts

--- debug.R
+++
@@ -1,9 +0,0 @@
-library(palmerpenguins)
-library(marginaleffects)
-library(nnet)
-dat = penguins
-dat$island <- relevel(dat$island, ref = "Torgersen")
-mod = multinom(island ~ bill_length_mm + flipper_length_mm, dat, hess = TRUE)
-predictions(mod) |> head()
-
-predictions(mod) |> attr("jacobian") |> head()
