0712108b72bacf7976a7758950e6577c2fb4532b
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-02 17:35:43 -0400

    minor

--- marginaleffects/__init__.py
+++ marginaleffects/__init__.py
@@ -3,4 +3,3 @@ from .datagrid import datagrid
 from .hypotheses import hypotheses
 from .predictions import avg_predictions, predictions
 from .slopes import avg_slopes, slopes
-from .testing import rdatasets
