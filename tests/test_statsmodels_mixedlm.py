e548ef25e6a6160515e1224ca24d91b32a0c6aa3
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-03 19:39:12 -0400

    tolerance

--- tests/test_statsmodels_mixedlm.py
+++ tests/test_statsmodels_mixedlm.py
@@ -13,22 +13,26 @@ mod = smf.mixedlm(formula = "Weight ~ Time * Litter", data = dat, groups=dat["Pi
 def test_predictions_01():
     unknown = predictions(mod)
     known = pl.read_csv("tests/r/test_statsmodels_mixedlm_predictions_01.csv")
-    assert_series_equal(known["estimate"], unknown["estimate"])
+    assert_series_equal(known["estimate"], unknown["estimate"], check_names = False)
+    assert_series_equal(known["std.error"], unknown["std_error"], check_names = False)
 
 
 def test_predictions_02():
     unknown = predictions(mod, by = "Cu")
     known = pl.read_csv("tests/r/test_statsmodels_mixedlm_predictions_02.csv")
-    assert_series_equal(known["estimate"], unknown["estimate"])
+    assert_series_equal(known["estimate"], unknown["estimate"], check_names = False)
+    assert_series_equal(known["std.error"], unknown["std_error"], check_names = False)
 
 
 def test_comparisons_01():
     unknown = comparisons(mod)
     known = pl.read_csv("tests/r/test_statsmodels_mixedlm_comparisons_01.csv", ignore_errors=True)
-    assert_series_equal(known["estimate"], unknown["estimate"], rtol = 3e-2)
+    assert_series_equal(known["estimate"], unknown["estimate"], rtol = 3e-2, check_names = False)
+    assert_series_equal(known["std.error"], unknown["std_error"], check_names = False)
 
 
 def test_comparisons_02():
     unknown = comparisons(mod, by = "Cu").sort(["term", "Cu"])
     known = pl.read_csv("tests/r/test_statsmodels_mixedlm_comparisons_02.csv").sort(["term", "Cu"])
-    assert_series_equal(known["estimate"], unknown["estimate"])
+    assert_series_equal(known["estimate"], unknown["estimate"], check_names = False)
+    assert_series_equal(known["std.error"], unknown["std_error"], check_names = False)
