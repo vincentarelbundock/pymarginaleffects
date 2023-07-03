4e97cf900a214b769c9e6258160380ca7817d972
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-03 07:41:09 -0400

    test cleanup

--- tests/test_statsmodels_ols.py
+++ tests/test_statsmodels_ols.py
@@ -1,7 +1,8 @@
 import polars as pl
+from polars.testing import assert_series_equal
 import statsmodels.formula.api as smf
 from marginaleffects import *
-from pytest import approx
+
 
 dat = pl.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv")
 dat = dat.with_columns(pl.col("cyl").cast(pl.Utf8))
@@ -11,31 +12,29 @@ mod = smf.ols("mpg ~ qsec * wt + cyl", data = dat).fit()
 def test_predictions_01():
     unknown = predictions(mod)
     known = pl.read_csv("tests/r/test_statsmodels_ols_predictions_01.csv")
-    assert known["estimate"].to_numpy() == approx(unknown["estimate"].to_numpy(), rel = 1e-4)
-    assert known["std.error"].to_numpy() == approx(unknown["std_error"].to_numpy(), rel = 1e-4)
+    assert_series_equal(unknown["estimate"], known["estimate"])
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names = False)
 
 
 def test_predictions_02():
     unknown = predictions(mod, by = "carb")
     known = pl.read_csv("tests/r/test_statsmodels_ols_predictions_02.csv")
-    assert known["estimate"].to_numpy() == approx(unknown["estimate"].to_numpy(), rel = 1e-4)
-    assert known["std.error"].to_numpy() == approx(unknown["std_error"].to_numpy(), rel = 1e-4)
+    assert_series_equal(unknown["estimate"], known["estimate"])
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names = False)
 
 
 def test_comparisons_01():
-    unknown = comparisons(mod)
-    known = pl.read_csv("tests/r/test_statsmodels_ols_comparisons_01.csv")
-    unknown = unknown.sort(["term", "contrast", "rowid"])
-    known = known.sort(["term", "contrast", "rowid"])
-    assert known["estimate"].to_numpy() == approx(unknown["estimate"].to_numpy(), rel = 1e-4)
-    assert known["std.error"].to_numpy() == approx(unknown["std_error"].to_numpy(), rel = 1e-4)
+    unknown = comparisons(mod).sort(["term", "contrast", "rowid"])
+    known = pl.read_csv("tests/r/test_statsmodels_ols_comparisons_01.csv").sort(["term", "contrast", "rowid"])
+    assert_series_equal(unknown["estimate"], known["estimate"])
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names = False)
 
 
 def test_comparisons_02():
     unknown = comparisons(mod, by = "carb").sort(["term", "carb"])
     known = pl.read_csv("tests/r/test_statsmodels_ols_comparisons_02.csv").sort(["term", "carb"])
-    assert known["estimate"].to_numpy() == approx(unknown["estimate"].to_numpy(), rel = 1e-4)
-    assert known["std.error"].to_numpy() == approx(unknown["std_error"].to_numpy(), rel = 1e-4)
+    assert_series_equal(unknown["estimate"], known["estimate"])
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names = False)
 
 
 def test_slopes_01():
@@ -43,12 +42,12 @@ def test_slopes_01():
     known = pl.read_csv("tests/r/test_statsmodels_ols_slopes_01.csv")
     unknown = unknown.sort(["term", "contrast", "rowid"])
     known = known.sort(["term", "contrast", "rowid"])
-    assert known["estimate"].to_numpy() == approx(unknown["estimate"].to_numpy(), rel = 2e-2)
-    assert known["std.error"].to_numpy() == approx(unknown["std_error"].to_numpy(), rel = 2e-2)
+    assert_series_equal(unknown["estimate"], known["estimate"], rtol = 1e-3)
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names = False, rtol = 1e-3)
 
 
 def test_slopes_02():
     unknown = slopes(mod, by = "carb").sort(["term", "carb"])
     known = pl.read_csv("tests/r/test_statsmodels_ols_slopes_02.csv").sort(["term", "carb"])
-    assert known["estimate"].to_numpy() == approx(unknown["estimate"].to_numpy(), rel = 1e-3)
-    assert known["std.error"].to_numpy() == approx(unknown["std_error"].to_numpy(), rel = 1e-4)
+    assert_series_equal(unknown["estimate"], known["estimate"])
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names = False)
