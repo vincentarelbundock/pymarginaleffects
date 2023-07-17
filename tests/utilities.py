00013862c7395d8d819d1e61e61a812603cd4607
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-17 17:09:49 -0400

    no more rpy2

--- tests/utilities.py
+++ tests/utilities.py
@@ -2,26 +2,6 @@ import re
 import numpy as np
 from marginaleffects import *
 import polars as pl
-from rpy2.robjects.packages import importr
-import rpy2.robjects as ro
-from rpy2.robjects import r, pandas2ri
-
-
-def pandas_to_r(df):
-    with (ro.default_converter + ro.pandas2ri.converter).context():
-        out = ro.conversion.get_conversion().py2rpy(df)
-    return out
-
-def polars_to_r(df):
-    return pandas_to_r(df.to_pandas())
-
-def r_to_pandas(df):
-    with (ro.default_converter + ro.pandas2ri.converter).context():
-        out = ro.conversion.get_conversion().rpy2py(df)
-    return out
-
-def r_to_polars(df):
-    return pl.from_pandas(r_to_pandas(df))
 
 
 def compare_r_to_py(r_obj, py_obj, tolr = 1e-3, tola = 1e-3, msg = ""):
@@ -40,14 +20,3 @@ def compare_r_to_py(r_obj, py_obj, tolr = 1e-3, tola = 1e-3, msg = ""):
             gap_abs = (a - b).abs().max()
             flag = gap_rel <= tolr or gap_abs <= tola
             assert flag, f"{msg} trel: {gap_rel}. tabs: {gap_abs}"
-
-
-def rdatasets(package, dataset, r = False):
-    url = f"https://vincentarelbundock.github.io/Rdatasets/csv/{package}/{dataset}.csv"
-    dat_py = pl.read_csv(url)
-    dat_py = dat_py.rename({"": "rownames"})
-    if r is False:
-        return dat_py
-    else:
-        dat_r = pandas_to_r(dat_py.to_pandas())
-        return dat_py, dat_r
