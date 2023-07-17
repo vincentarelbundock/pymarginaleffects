851ffceb5f0d4b827fef8252986cf422bea6aa5b
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-17 13:53:15 -0400

    test_comparisons

--- tests/test_comparisons.py
+++ tests/test_comparisons.py
@@ -21,66 +21,65 @@ dat = dat \
 mod = smf.ols("Literacy ~ Pop1831 * Desertion", dat).fit()
 
 
-# def test_difference():
-#     cmp_py = comparisons(mod, comparison = "differenceavg").sort("term")
-#     cmp_r = pl.read_csv("tests/r/test_comparisons_01.csv").sort("term")
-#     assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-#     assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False)
-#     cmp_py = comparisons(mod, comparison = "difference").sort("term", "rowid")
-#     cmp_r = pl.read_csv("tests/r/test_comparisons_02.csv").sort("term", "rowid")
-#     assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-#     assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-4)
+def test_difference():
+    cmp_py = comparisons(mod, comparison = "differenceavg").sort("term")
+    cmp_r = pl.read_csv("tests/r/test_comparisons_01.csv").sort("term")
+    assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
+    assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
+    cmp_py = comparisons(mod, comparison = "difference").sort("term", "rowid")
+    cmp_r = pl.read_csv("tests/r/test_comparisons_02.csv").sort("term", "rowid")
+    assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
+    assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
 
 
 def test_comparison_simple():
     est = [k for k in estimands.keys() if not re.search("x|wts", k)]
-    est = ["lnratio", "difference"]
     for e in est:
         cmp_py = comparisons(mod, comparison = e).sort("term")
         cmp_r = pl.read_csv(f"tests/r/test_comparisons_03_{e}.csv").sort("term")
         if cmp_r.shape[1] == 170:
             raise ValueError("R and Python results are not the same")
-        assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-        assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
+        assert_series_equal(cmp_py["estimate"], cmp_r["estimate"], rtol = 1e-3)
+        assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-2)
 
 
-# def test_by():
-#     cmp_py = comparisons(mod, comparison = "differenceavg", by = "Region").sort("term", "Region")
-#     cmp_r = pl.read_csv("tests/r/test_comparisons_04.csv").sort("term", "Region")
-#     assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-#     assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
+def test_by():
+    cmp_py = comparisons(mod, comparison = "differenceavg", by = "Region").sort("term", "Region")
+    cmp_r = pl.read_csv("tests/r/test_comparisons_04.csv").sort("term", "Region")
+    assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
+    assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
 
 
-# def test_HC3():
-#     cmp_py = comparisons(mod, comparison = "differenceavg", vcov = "HC3").sort("term")
-#     cmp_r = pl.read_csv("tests/r/test_comparisons_05.csv").sort("term")
-#     assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-#     assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
+def test_HC3():
+    cmp_py = comparisons(mod, comparison = "differenceavg", vcov = "HC3").sort("term")
+    cmp_r = pl.read_csv("tests/r/test_comparisons_05.csv").sort("term")
+    assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
+    assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-3)
 
 
-# def test_bare_minimum():
-#     fit = smf.ols("Literacy ~ Pop1831 * Desertion + Bool + Bin + Char", dat).fit()
-#     assert type(comparisons(fit)) == pl.DataFrame
-#     assert type(comparisons(fit, variables = "Pop1831", comparison = "differenceavg")) == pl.DataFrame
-#     assert type(comparisons(fit, variables = "Pop1831", comparison = "difference").head()) == pl.DataFrame
-#     assert type(comparisons(fit, variables = "Pop1831", comparison = "ratio").head()) == pl.DataFrame
-#     assert type(comparisons(fit, variables = "Pop1831", comparison = "difference", by = "Region")) == pl.DataFrame
-#     assert type(comparisons(fit, vcov = False, comparison = "differenceavg")) == pl.DataFrame
-#     assert type(comparisons(fit, vcov = "HC3", comparison = "differenceavg")) == pl.DataFrame
-#     assert type(comparisons(fit)) == pl.DataFrame
-#     assert type(comparisons(fit, variables = {"Char": "sequential"})) == pl.DataFrame
-#     assert type(comparisons(fit, variables = "Pop1831")) == pl.DataFrame
-#     assert type(comparisons(fit, variables = ["Pop1831", "Desertion"])) == pl.DataFrame
-#     assert type(comparisons(fit, variables = {"Pop1831": 1000, "Desertion": 2})) == pl.DataFrame
-#     assert type(comparisons(fit, variables = {"Pop1831": [100, 2000]})) == pl.DataFrame
+def test_difference_wts():
+    cmp_py = comparisons(mod, variables = "Desertion", by = "Region", wts = "Literacy")
+    cmp_r = pl.read_csv("tests/r/test_comparisons_06.csv")
+    assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
+    assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-4)
+    cmp_py = comparisons(mod, variables = "Desertion", by = "Region")
+    cmp_r = pl.read_csv("tests/r/test_comparisons_07.csv")
+    assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
+    assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-4)
 
 
-# def test_difference_wts():
-#     cmp_py = comparisons(mod, variables = "Desertion", by = "Region", wts = "Literacy")
-#     cmp_r = pl.read_csv("tests/r/test_comparisons_06.csv")
-#     assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-#     assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-4)
-#     cmp_py = comparisons(mod, variables = "Desertion", by = "Region")
-#     cmp_r = pl.read_csv("tests/r/test_comparisons_07.csv")
-#     assert_series_equal(cmp_py["estimate"], cmp_r["estimate"])
-#     assert_series_equal(cmp_py["std_error"], cmp_r["std.error"], check_names = False, rtol = 1e-4)
+def test_bare_minimum():
+    fit = smf.ols("Literacy ~ Pop1831 * Desertion + Bool + Bin + Char", dat).fit()
+    assert type(comparisons(fit)) == pl.DataFrame
+    assert type(comparisons(fit, variables = "Pop1831", comparison = "differenceavg")) == pl.DataFrame
+    assert type(comparisons(fit, variables = "Pop1831", comparison = "difference").head()) == pl.DataFrame
+    assert type(comparisons(fit, variables = "Pop1831", comparison = "ratio").head()) == pl.DataFrame
+    assert type(comparisons(fit, variables = "Pop1831", comparison = "difference", by = "Region")) == pl.DataFrame
+    assert type(comparisons(fit, vcov = False, comparison = "differenceavg")) == pl.DataFrame
+    assert type(comparisons(fit, vcov = "HC3", comparison = "differenceavg")) == pl.DataFrame
+    assert type(comparisons(fit)) == pl.DataFrame
+    assert type(comparisons(fit, variables = {"Char": "sequential"})) == pl.DataFrame
+    assert type(comparisons(fit, variables = "Pop1831")) == pl.DataFrame
+    assert type(comparisons(fit, variables = ["Pop1831", "Desertion"])) == pl.DataFrame
+    assert type(comparisons(fit, variables = {"Pop1831": 1000, "Desertion": 2})) == pl.DataFrame
+    assert type(comparisons(fit, variables = {"Pop1831": [100, 2000]})) == pl.DataFrame
