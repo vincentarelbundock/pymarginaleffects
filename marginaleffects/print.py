ea59ba09c842502e5a6249ca31d8c696b29b16b5
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-23 08:41:00 -0400

    Revert "printout still not working"
    This reverts commit 11733081208ff7ea69ac92416f5abbab58aaeb5b.

--- marginaleffects/print.py
+++ marginaleffects/print.py
@@ -1,5 +1,15 @@
 def get_printout(x, by = None, conf_int = 0.95):
-    mapping = {
+    column_mapping = {
+        "term": "Term",
+        "contrast": "Contrast",
+    }
+    
+    if by is not None:
+        new_mapping = {key: key for key in by}
+    else:
+        new_mapping = {}
+    column_mapping.update(new_mapping)
+    new_mapping = {
         "term": "Term",
         "contrast": "Contrast",
         "estimate": "Estimate",
@@ -7,22 +17,13 @@ def get_printout(x, by = None, conf_int = 0.95):
         "statistic": "z",
         "p_value": "P(>|z|)",
         "s_value": "S",
-        "conf_low": f"{(1 - conf_int) / 2 * 100:.1f}%",
-        "conf_high": f"{(1 - (1 - conf_int) / 2) * 100:.1f}%",
+        "conf_low": f"{(1 - conf_int) / 2 * 100:.1f}",
+        "conf_high": f"{(1 - (1 - conf_int) / 2) * 100:.1f}",
     }
-    if isinstance(by, list):
-        valid = by + list(mapping.keys())
-    elif isinstance(by, str):
-        valid = [by] + list(mapping.keys())
-    else:
-        valid = list(mapping.keys())
-    valid = [column for column in valid if column in x.columns]
-    mapping = {key: value for key, value in mapping.items() if key in valid}
-    out = x \
-        .select(valid) \
-        .rename(mapping)
+    column_mapping.update(new_mapping)
+    existing_columns = [column for column in column_mapping.keys() if column in x.columns]
+    new_names = {column: column_mapping[column] for column in existing_columns}
+    out = x.select(existing_columns).rename(new_names)
     out = out.__str__()
-    out = out + "\nColumns: " + ", ".join(x.columns)
-    def fn():
-        return out
-    return fn
+    out = out + "\n\nColumns: " + ", ".join(x.columns)
+    return out
