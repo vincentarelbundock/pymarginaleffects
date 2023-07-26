bd4f0304177c08f1d23bd0fb86be04e261c4509b
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-07-26 15:48:55 -0400

    new tests

--- tests/test_buildplot.py
+++ tests/test_buildplot.py
@@ -5,13 +5,30 @@ from marginaleffects.buildplot import build_plot
 from .utilities import *
 
 
-df = pl.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/HistData/Guerry.csv", null_values = "NA").drop_nulls()
-df = df \
+df = pl.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/HistData/Guerry.csv", null_values = "NA") \
+    .drop_nulls() \
     .with_columns(pl.Series(range(df.shape[0])).alias("row_id")) \
     .sort("Region", "row_id")
-mod_py = smf.ols("Literacy ~ Pop1831 * Desertion", df).fit()
+mod = smf.ols("Literacy ~ Pop1831 * Desertion", df).fit()
+
 
 def test_build_plot():
-    con_list = ['dept', 'Region']
-    con_dict = {'dept' : [1,3] , 'Region' : 1, 'Department' : 2}
-    build_plot(mod_py, con_list)
+    con = {'dept' : [1, 3] , 'Region' : "W", 'Department' : "Allier"}
+    assert build_plot(mod, con).shape[0] == 2
+    con = "Area"
+    assert build_plot(mod, con).shape[0] == 100
+    con = ["Area"]
+    assert build_plot(mod, con).shape[0] == 100
+    con = {"Area": None, "Region": "W"}
+    assert build_plot(mod, con).shape[0] == 100
+    con = ["Region", "Area"]
+    assert build_plot(mod, con).shape[0] == 25
+    con = ["Region", "Area", "Pop1831"]
+    assert build_plot(mod, con).shape[0] == 125
+
+
+
+
+
+           
+           ["Distance", "Region"])
