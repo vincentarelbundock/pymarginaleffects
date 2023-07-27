875fe52eb10ac5db655cb99315514b105076ea1d
Adrien Lamarche <adrien.lamarche@protonmail.com>
2023-07-27 09:52:13 -0400

    corrections to step 1

--- marginaleffects/buildplot.py
+++ marginaleffects/buildplot.py
@@ -5,53 +5,64 @@ import sys
 from .utils import get_modeldata, get_variable_type
 from .datagrid import datagrid
 
-import statsmodels.formula.api as smf
-
-
-
 
 def build_plot(model, condition):
 
+
     modeldata = get_modeldata(model)
+
+    if isinstance(condition, str):
+        condition = [condition]
 
     assert 1 <= len(condition) <= 3, f"Lenght of condition must be inclusively between 1 and 3. Got : {len(condition)}"
 
-    to_datagrid = {}
 
+    to_datagrid = {}
+    first_key = ""  # special case when the first element is numeric
 
     if isinstance(condition, list):
-
         assert all(ele in modeldata.columns for ele in condition), "All elements of condition must be columns of the model"
-
-        for i, ele in enumerate(condition):
-            variable_type = get_variable_type(ele, modeldata)
-
-            if variable_type == 'numeric':
-                if i == 0:
-                    to_datagrid[ele] = np.linspace(modeldata[ele].min(), modeldata[ele].max(), 100).tolist()
-                else:
-                    to_datagrid[ele] = np.percentile(modeldata[ele], [0, 25, 50, 75, 100], method="midpoint").tolist()
-
-            elif variable_type == 'boolean' or variable_type == 'character':
-                to_datagrid[ele] = modeldata[ele].unique().to_list()
-                assert len(to_datagrid[ele]) <= 10, f"Character type variables of more than 10 unique values are not supported. {ele} variable has {len(to_datagrid[ele])} unique values."
-
-            # binary ?
-
+        first_key = condition[0]
+        to_datagrid = {key: None for key in condition}
 
     elif isinstance(condition, dict):
         assert all(key in modeldata.columns for key in condition.keys()), "All keys of condition must be columns of the model"
-
+        first_key = next(iter(condition))
         to_datagrid = condition    
 
 
-    # uncomfortable. did you already solve this problem?
-    # also not sure if I use datagrid the right way
+
+    for key, value in to_datagrid.items():
+
+        variable_type = get_variable_type(key, modeldata)
+
+        # Check type of user-supplied dict values
+        if value is not None:
+            test_df = pl.DataFrame({key: value})
+            assert variable_type == get_variable_type(key, test_df), f"Supplied data type of {key} column ({get_variable_type(key, test_df)}) does not match the type of the variable ({variable_type})."
+            continue
+
+        if variable_type == 'numeric':
+            if key == first_key:
+                to_datagrid[key] = np.linspace(modeldata[key].min(), modeldata[key].max(), 100).tolist()
+            else:
+                to_datagrid[key] = np.percentile(modeldata[key], [0, 25, 50, 75, 100], method="midpoint").tolist()
+
+        elif variable_type == 'boolean' or variable_type == 'character':
+            to_datagrid[key] = modeldata[key].unique().to_list()
+            assert len(to_datagrid[key]) <= 10, f"Character type variables of more than 10 unique values are not supported. {ele} variable has {len(to_datagrid[ele])} unique values."
+
+
+
     dt_code = "datagrid(newdata=modeldata"
     for key, value in to_datagrid.items():
-        dt_code += ", " + key + "=" + str(value)
+        dt_code += ", " + key + "="
+        if isinstance(value, str):
+            dt_code += "'" + value + "'"
+        else:
+            dt_code += str(value)
     dt_code += ")"
 
     exec("global dt; dt = " + dt_code)
-    
+
     return dt
--- tests/test_buildplot.py
+++ tests/test_buildplot.py
@@ -6,8 +6,8 @@ from .utilities import *
 
 
 df = pl.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/HistData/Guerry.csv", null_values = "NA") \
-    .drop_nulls() \
-    .with_columns(pl.Series(range(df.shape[0])).alias("row_id")) \
+    .drop_nulls()
+df = df.with_columns(pl.Series(range(df.shape[0])).alias("row_id")) \
     .sort("Region", "row_id")
 mod = smf.ols("Literacy ~ Pop1831 * Desertion", df).fit()
 
@@ -19,16 +19,9 @@ def test_build_plot():
     assert build_plot(mod, con).shape[0] == 100
     con = ["Area"]
     assert build_plot(mod, con).shape[0] == 100
-    con = {"Area": None, "Region": "W"}
-    assert build_plot(mod, con).shape[0] == 100
     con = ["Region", "Area"]
     assert build_plot(mod, con).shape[0] == 25
     con = ["Region", "Area", "Pop1831"]
     assert build_plot(mod, con).shape[0] == 125
-
-
-
-
-
-           
-           ["Distance", "Region"])
+    con = {"Area": None, "Region": "W"}
+    assert build_plot(mod, con).shape[0] == 100
