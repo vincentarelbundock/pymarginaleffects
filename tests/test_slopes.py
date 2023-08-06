e722899b3e7cc4f0bf9f908ad943359fcc17f865
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-08-06 16:37:15 -0400

    bugfix

--- tests/test_slopes.py
+++ tests/test_slopes.py
@@ -1,6 +1,6 @@
 import statsmodels.formula.api as smf
 from marginaleffects import *
-# from .utilities import *
+from .utilities import *
 from marginaleffects.comparisons import estimands
 from polars.testing import assert_series_equal
 import polars as pl
@@ -32,7 +32,9 @@ def test_slopes_padding():
     assert s.shape[0] == 3
 
 
-dat = pl.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv") \
-    .with_columns(pl.col("cyl").cast(pl.Utf8))
-mod = smf.ols("mpg ~ cyl + hp", dat).fit()
-s = slopes(mod, newdata = "mean", variables = "hp")
+def test_bug_newdata_variables():
+    dat = pl.read_csv("https://vincentarelbundock.github.io/Rdatasets/csv/datasets/mtcars.csv") \
+        .with_columns(pl.col("cyl").cast(pl.Utf8))
+    mod = smf.ols("mpg ~ cyl + hp", dat).fit()
+    s = slopes(mod, newdata = "mean", variables = "hp")
+    assert s.shape[0] == 1
