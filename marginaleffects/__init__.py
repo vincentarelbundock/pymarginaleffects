ae4cd2a98625da28e42abd7619f8556b08c50ca4
Adrien Lamarche <adrien.lamarche@protonmail.com>
2023-11-24 12:39:13 -0500

    import plot functions in __init__.py

--- marginaleffects/__init__.py
+++ marginaleffects/__init__.py
@@ -3,3 +3,6 @@ from .datagrid import datagrid, datagridcf
 from .hypotheses import hypotheses
 from .predictions import avg_predictions, predictions
 from .slopes import avg_slopes, slopes
+from .plot_predictions import plot_predictions
+from .plot_comparisons import plot_comparisons
+from .plot_slopes import plot_slopes
