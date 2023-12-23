ca3046d962400b03292ba55dd988c8c38e96071f
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-12-23 13:08:12 -0500

    optional statsmodels

--- marginaleffects/sanitize_model.py
+++ marginaleffects/sanitize_model.py
@@ -2,10 +2,20 @@ from .model_abstract import ModelAbstract, ModelStatsmodels
 
 
 def sanitize_model(model):
-    # TODO: other than statsmodels
     if model is None:
         return model
 
-    if not isinstance(model, ModelAbstract):
-        model = ModelStatsmodels(model)
-    return model
+    if isinstance(model, ModelAbstract):
+        return model
+
+    try:
+        import statsmodels.base.wrapper as smw
+
+        if isinstance(model, smw.ResultsWrapper):
+            return ModelStatsmodels(model)
+    except ImportError:
+        pass
+
+    raise ValueError(
+        "Unknown model type. Try installing the 'statsmodels' package or file an issue at https://github.com/vincentarelbundock/pymarginaleffects."
+    )
