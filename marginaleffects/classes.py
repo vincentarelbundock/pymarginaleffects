dd66f61bc3dd903cb7f50a87a2e04d8fd95b129a
LamAdr <102169875+LamAdr@users.noreply.github.com>
2024-03-05 17:08:54 -0500

    Joint hypotheses (#94)

--- marginaleffects/classes.py
+++ marginaleffects/classes.py
@@ -3,7 +3,14 @@ import polars as pl
 
 class MarginaleffectsDataFrame(pl.DataFrame):
     def __init__(
-        self, data=None, by=None, conf_level=0.95, jacobian=None, newdata=None
+        self,
+        data=None,
+        by=None,
+        conf_level=0.95,
+        jacobian=None,
+        newdata=None,
+        mapping=None,
+        print_head="",
     ):
         if isinstance(data, pl.DataFrame):
             self._df = data._df
@@ -14,42 +21,52 @@ class MarginaleffectsDataFrame(pl.DataFrame):
                 self.datagrid_explicit = newdata.datagrid_explicit
             else:
                 self.datagrid_explicit = []
+
+            self.print_head = print_head
+
+            default_mapping = {
+                "term": "Term",
+                "contrast": "Contrast",
+                "estimate": "Estimate",
+                "std_error": "Std.Error",
+                "statistic": "z",
+                "p_value": "P(>|z|)",
+                "s_value": "S",
+            }
+            if mapping is None:
+                self.mapping = default_mapping
+            else:
+                for key, val in default_mapping.items():
+                    if key not in mapping.keys():
+                        mapping[key] = val
+                self.mapping = mapping
+
             return
         super().__init__(data)
 
     def __str__(self):
-        mapping = {
-            "term": "Term",
-            "contrast": "Contrast",
-            "estimate": "Estimate",
-            "std_error": "Std.Error",
-            "statistic": "z",
-            "p_value": "P(>|z|)",
-            "s_value": "S",
-        }
-
         if hasattr(self, "conf_level"):
-            mapping["conf_low"] = f"{(1 - self.conf_level) / 2 * 100:.1f}%"
-            mapping["conf_high"] = f"{(1 - (1 - self.conf_level) / 2) * 100:.1f}%"
+            self.mapping["conf_low"] = f"{(1 - self.conf_level) / 2 * 100:.1f}%"
+            self.mapping["conf_high"] = f"{(1 - (1 - self.conf_level) / 2) * 100:.1f}%"
         else:
-            mapping["conf_low"] = "["
-            mapping["conf_high"] = "]"
+            self.mapping["conf_low"] = "["
+            self.mapping["conf_high"] = "]"
 
         if hasattr(self, "by"):
             if self.by is None:
-                valid = list(mapping.keys())
+                valid = list(self.mapping.keys())
             elif self.by is True:
-                valid = list(mapping.keys())
+                valid = list(self.mapping.keys())
             elif self.by is False:
-                valid = list(mapping.keys())
+                valid = list(self.mapping.keys())
             elif isinstance(self.by, list):
-                valid = self.by + list(mapping.keys())
+                valid = self.by + list(self.mapping.keys())
             elif isinstance(self.by, str):
-                valid = [self.by] + list(mapping.keys())
+                valid = [self.by] + list(self.mapping.keys())
             else:
                 raise ValueError("by must be None or a string or a list of strings")
         else:
-            valid = list(mapping.keys())
+            valid = list(self.mapping.keys())
 
         valid = self.datagrid_explicit + valid
         valid = [x for x in valid if x in self.columns]
@@ -58,11 +75,12 @@ class MarginaleffectsDataFrame(pl.DataFrame):
         valid = dict.fromkeys(valid)
         valid = list(valid.keys())
 
-        mapping = {key: mapping[key] for key in mapping if key in valid}
-        tmp = self.select(valid).rename(mapping)
+        out = self.print_head
+        self.mapping = {key: self.mapping[key] for key in self.mapping if key in valid}
+        tmp = self.select(valid).rename(self.mapping)
         for col in tmp.columns:
             if tmp[col].dtype.is_numeric():
                 tmp = tmp.with_columns(pl.col(col).map_elements(lambda x: f"{x:.3g}"))
-        out = tmp.__str__()
+        out += tmp.__str__()
         out = out + f"\n\nColumns: {', '.join(self.columns)}\n"
         return out
