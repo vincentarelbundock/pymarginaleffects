24f7d1c4e313bbf0dec681373bc4195b635afc71
artiom.matvei <artiommatvei@gmail.com>
2024-11-18 11:38:27 -0500

    wip

--- tests/statsmodels/test_statsmodels_mnlogit.py
+++ tests/statsmodels/test_statsmodels_mnlogit.py
@@ -82,41 +82,59 @@ def test_predictions_02():
     assert_series_equal(known["estimate"], unknown["estimate"], rtol=1e-1)
 
 
-# def test_comparisons_01():
-#     penguins_clean = penguins_with_nulls.select(
-#         ["island", "bill_length_mm", "flipper_length_mm"]
-#     ).drop_nulls()
+def test_comparisons_01():
+    penguins_clean = penguins_with_nulls.select(
+        ["island", "bill_length_mm", "flipper_length_mm"]
+    ).drop_nulls()
 
-#     # Define island categories and create a mapping
-#     island_categories = ["Biscoe", "Dream", "Torgersen"]
-#     island_mapping = {island: code for code, island in enumerate(island_categories)}
+    # Define island categories and create a mapping
+    island_categories = ["Biscoe", "Dream", "Torgersen"]
+    island_mapping = {island: code for code, island in enumerate(island_categories)}
 
-#     # Map 'island' to integer codes
-#     penguins_clean = penguins_clean.with_columns(
-#         pl.col("island").replace_strict(island_mapping)
-#     )
+    # Map 'island' to integer codes
+    penguins_clean = penguins_clean.with_columns(
+        pl.col("island").replace_strict(island_mapping)
+    )
 
-#     mod = smf.mnlogit(
-#         "island ~ bill_length_mm + flipper_length_mm", data=penguins_clean
-#     ).fit()
-#     unknown = (
-#         comparisons(mod)
-#         .with_columns(pl.col("group").replace(island_mapping))
-#         .sort(["rowid", "term", "group"])
-#     )
-#     known = (
-#         pl.read_csv("tests/r/test_statsmodels_mnlogit_comparisons_01.csv")
-#         .with_columns(pl.col("group").replace(island_mapping))
-#         .sort(["rowid", "term", "group"])
-#         )
-#     print(known)
-#     print(unknown)
-#     assert_series_equal(known["estimate"].head(), unknown["estimate"].head(), rtol=2)
+    mod = smf.mnlogit(
+        "island ~ bill_length_mm + flipper_length_mm", data=penguins_clean
+    ).fit()
+    unknown = (
+        comparisons(mod)
+        .with_columns(pl.col("group").replace(island_mapping))
+        .sort(["rowid", "term", "group"])
+    )
+    known = (
+        pl.read_csv("tests/r/test_statsmodels_mnlogit_comparisons_01.csv")
+        .with_columns(pl.col("group").replace(island_mapping))
+        .sort(["rowid", "term", "group"])
+    )
+    new_column_names = {col: col.replace('.', '_') for col in known.columns}
+    known = known.rename(new_column_names)
+    print(known.head())
+    print(unknown.head())
+    print(compare_polars_tables(known, unknown, index=0))
+    assert_series_equal(known["estimate"].head(), unknown["estimate"].head(), rtol=2)
 
-#     unknown = comparisons(mod)
-#     known = pl.read_csv("tests/r/test_statsmodels_mnlogit_comparisons_01.csv")
-#     assert_series_equal(known["estimate"], unknown["estimate"], rtol=1e-2)
+    unknown = comparisons(mod)
+    known = pl.read_csv("tests/r/test_statsmodels_mnlogit_comparisons_01.csv")
+    assert_series_equal(known["estimate"], unknown["estimate"], rtol=1e-2)
 
+# Function to print visual comparison
+def compare_polars_tables(known, unknown, index=0):
+    headers = ["Column", "Table known Value", "Table unknown Value", "Difference"]
+    row_format = "{:<25} {:<25} {:<25} {:<15}"
+    
+    print(row_format.format(*headers))
+    print("-" * 60)
+    
+    for col in known.columns:
+        val1 = known[col][index]
+        val2 = unknown[col][index]
+        difference = "Yes" if val1 != val2 else "No"
+        
+        print(row_format.format(col, val1, val2, difference))
+    print(row_format.format("index", index, index, "No"))
 
 # # @pytest.mark.skip(reason="statsmodels vcov is weird")
 # def test_comparisons_02():
