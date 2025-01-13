493a17491d3c0f38123ca3176fba2d0dbc32959e
johannes <johannes.weytjens@peregrinus.be>
2025-01-13 14:46:30 +0100

    test: fix linearmodels test and add atol to equality test

--- tests/test_linearmodels_panelols.py
+++ tests/test_linearmodels_panelols.py
@@ -1,65 +1,71 @@
 import polars as pl
-from marginaleffects import *
+import marginaleffects as me
 from polars.testing import assert_series_equal
 from tests.conftest import wage_panel_pd
 from linearmodels.panel import PanelOLS
-from marginaleffects import fit_linearmodels
 
 formula = "lwage ~ exper * hours * educ * married - 1"
 data = wage_panel_pd
-mod = fit_linearmodels(formula, data, engine=PanelOLS)
+mod = me.fit_linearmodels(formula, data, engine=PanelOLS)
 
 
 def test_predictions_01():
-    unknown = predictions(mod)
-    known = pl.read_csv("tests/r/test_linearmodels_panelols_predictions_01.csv")
-    assert_series_equal(unknown["estimate"], known["estimate"])
-    assert_series_equal(unknown["std_error"], known["std_error"], check_names=False)
+    unknown = me.predictions(mod)
+    known = pl.read_csv("tests/r/test_feols_linearmodels_panelols_predictions_01.csv")
+    assert_series_equal(unknown["estimate"], known["estimate"], atol=1e-7)
+    assert_series_equal(
+        unknown["std_error"], known["std.error"], check_names=False, atol=1e-7
+    )
 
 
 def test_predictions_02():
-    unknown = predictions(mod, by="married")
-    known = pl.read_csv("tests/r/test_linearmodels_panelols_predictions_02.csv")
-    assert_series_equal(unknown["estimate"], known["estimate"])
-    assert_series_equal(unknown["std_error"], known["std_error"], check_names=False)
+    unknown = me.predictions(mod, by="married")
+    known = pl.read_csv("tests/r/test_feols_linearmodels_panelols_predictions_02.csv")
+    assert_series_equal(unknown["estimate"], known["estimate"], atol=1e-7)
+    assert_series_equal(
+        unknown["std_error"], known["std.error"], check_names=False, atol=1e-7
+    )
 
 
 def test_comparisons_01():
-    unknown = comparisons(mod).sort(["term", "contrast", "rowid"])
-    known = pl.read_csv("tests/r/test_linearmodels_panelols_comparisons_01.csv").sort(
-        ["term", "contrast", "rowid"]
+    unknown = me.comparisons(mod).sort(["term", "contrast", "rowid"])
+    known = pl.read_csv(
+        "tests/r/test_feols_linearmodels_panelols_comparisons_01.csv"
+    ).sort(["term", "contrast", "rowid"])
+    assert_series_equal(unknown["estimate"], known["estimate"], atol=1e-7)
+    assert_series_equal(
+        unknown["std_error"], known["std.error"], check_names=False, atol=1e-7
     )
-    assert_series_equal(unknown["estimate"], known["estimate"])
-    assert_series_equal(unknown["std_error"], known["std_error"], check_names=False)
 
 
 def test_comparisons_02():
-    unknown = comparisons(mod, by="married").sort(["term", "married"])
-    known = pl.read_csv("tests/r/test_linearmodels_panelols_comparisons_02.csv").sort(
-        ["term", "married"]
-    )
+    unknown = me.comparisons(mod, by="married").sort(["term", "married"])
+    known = pl.read_csv(
+        "tests/r/test_feols_linearmodels_panelols_comparisons_02.csv"
+    ).sort(["term", "married"])
     assert_series_equal(unknown["estimate"], known["estimate"])
-    assert_series_equal(unknown["std_error"], known["std_error"], check_names=False)
+    assert_series_equal(unknown["std_error"], known["std.error"], check_names=False)
 
 
 def test_slopes_01():
-    unknown = slopes(mod)
-    known = pl.read_csv("tests/r/test_linearmodels_panelols_slopes_01.csv")
+    unknown = me.slopes(mod)
+    known = pl.read_csv("tests/r/test_feols_linearmodels_panelols_slopes_01.csv")
     unknown = unknown.sort(["term", "contrast", "rowid"])
     known = known.sort(["term", "contrast", "rowid"])
-    assert_series_equal(unknown["estimate"], known["estimate"], rtol=1e-4)
+    assert_series_equal(unknown["estimate"], known["estimate"], atol=1e-7)
     # TODO: bad tolerance
     assert_series_equal(
-        unknown["std_error"], known["std_error"], check_names=False, rtol=1e-1
+        unknown["std_error"], known["std.error"], check_names=False, atol=1e-2
     )
 
 
 def test_slopes_02():
-    unknown = slopes(mod, by="married").sort(["term", "married"])
-    known = pl.read_csv("tests/r/test_linearmodels_panelols_slopes_02.csv").sort(
+    unknown = me.slopes(mod, by="married").sort(["term", "married"])
+    known = pl.read_csv("tests/r/test_feols_linearmodels_panelols_slopes_02.csv").sort(
         ["term", "married"]
     )
-    assert_series_equal(unknown["estimate"], known["estimate"])
+    assert_series_equal(unknown["estimate"], known["estimate"], atol=1e-7)
+    # TODO: bad tolerance
     assert_series_equal(
-        unknown["std_error"], known["std_error"], check_names=False, rtol=1e-2
+        unknown["std_error"], known["std.error"], check_names=False, atol=1e-4
     )
