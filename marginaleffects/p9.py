e2e39c19ae3673f97b5d30ffc22496b9a04328e5
Artiom Matvei <artiommatvei@gmail.com>
2025-04-09 20:55:02 -0400

    removed no longer needed comments

--- marginaleffects/p9.py
+++ marginaleffects/p9.py
@@ -19,17 +19,13 @@ def plot_common(model, dt, y_label, var_list, gray=False):
     discrete = model.get_variable_type()[var_list[0]] not in ["numeric", "integer"]
     interval = "conf_low" in dt.columns
 
+    # treat all variables except x-axis as categorical
     if len(var_list) > 1:
-        for i in range(len(var_list) - 1, 0, -1):  # because .pop()
-            # treat all variables except x-axis as categorical
+        for i in range(1, len(var_list)):
             if dt[var_list[i]].dtype.is_numeric() and i != 0 and i != 1:
                 dt = dt.with_columns(pl.col(var_list[i]))
             elif dt[var_list[i]].dtype != pl.Categorical:
                 dt = dt.with_columns(pl.col(var_list[i]).cast(pl.Utf8))
-
-            # unique values do not get a distinct aesthetic/geom/facet
-            if dt[var_list[i]].unique().len() == 1:
-                var_list.pop(i)
 
     # aes
     # mapping = {"x": var_list[0], "y": y_label}  # proposed change to make y axis label correspond to R  but needs some debugging
@@ -47,24 +43,24 @@ def plot_common(model, dt, y_label, var_list, gray=False):
                 p = p + geom_pointrange(
                     aes(shape=var_list[1])
                     if gray
-                    else aes(color=var_list[1]),  # so this works correctly for gray
+                    else aes(color=var_list[1]),
                     position=position_dodge(width=0.1),
                 )
             else:
                 p = (
                     p + geom_pointrange()
-                )  # this does not need grayscale as it does not display colors anyways, run  discrete_interval_len1 to see
+                )  
         else:
             p = (
                 p + geom_point()
-            )  # this does not need grayscale as it does not display colors anyways, run  discrete_not_interval to see
+            ) 
     else:
         if interval:
             if len(var_list) > 1:
                 p = p + geom_ribbon(
                     aes(
                         fill=var_list[1]
-                    ),  # fixed, run experiment not_discrete_interval_len2 to see
+                    ),
                     alpha=0.2,
                 )
                 if gray:
@@ -74,8 +70,8 @@ def plot_common(model, dt, y_label, var_list, gray=False):
             else:
                 p = (
                     p + geom_ribbon(alpha=0.2)
-                )  # this does not need grayscale as it does not display colors anyways, run  not_discrete_interval_len1 to see
-        if len(var_list) > 1:  # we are here <----
+                )
+        if len(var_list) > 1:
             if gray:
                 # get the number of unique values in the column "var_list[1]"
                 unique_values = dt[var_list[1]].unique().len()
@@ -97,7 +93,7 @@ def plot_common(model, dt, y_label, var_list, gray=False):
         else:
             p = (
                 p + geom_line()
-            )  # this does not need grayscale as it does not display colors anyways, run  not_discrete_interval_len1 to see
+            )
 
     if len(var_list) == 3:
         p = p + facet_wrap(f"~ {var_list[2]}")
